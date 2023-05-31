0108635707beca5a6a74603e41a72b8d859afd93
retaj123123 <135163859+retaj123123@users.noreply.github.com>
2023-05-31 21:44:31 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,32 @@
+def on_button_pressed_a():
+    basic.shw_icon(IconNames.Heart)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_gesture_shake():
+    global h 
+    for index in range(4)
+        pass
+    basic.show_number(h)
+    h += 2
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
+def on_button_pressed_ab():
+    if g >=90:
+    basic.show_string("amazing")
+elif g >= 50:
+    basic.show_string("traversed")
+    else:
+    basic.show_string("Good luck")
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    global m
+    m += 1
+    basic.show_number(m)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+m = 0
+h = 0
+g = 0
+basic.show_string("retaj")
+g = 25
