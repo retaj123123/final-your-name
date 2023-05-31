dc14e5c0127cd71bd2bef925a81007efe15760db
retaj123123 <135163859+retaj123123@users.noreply.github.com>
2023-05-31 21:48:47 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
-def on_button_pressed_a():
+default on_button_pressed_a():
     basic.shw_icon(IconNames.Heart)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
-def on_gesture_shake():
+default on_gesture_shake():
     global h 
     for index in range(4)
         pass
@@ -10,7 +10,7 @@ def on_gesture_shake():
     h += 2
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
 
-def on_button_pressed_ab():
+default on_button_pressed_ab():
     if g >=90:
     basic.show_string("amazing")
 elif g >= 50:
@@ -19,7 +19,7 @@ elif g >= 50:
     basic.show_string("Good luck")
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
-def on_button_pressed_b():
+default on_button_pressed_b():
     global m
     m += 1
     basic.show_number(m)
